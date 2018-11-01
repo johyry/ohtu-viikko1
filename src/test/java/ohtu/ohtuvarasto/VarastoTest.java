b4add70d8d4992d62f73fe22839f26256edd2920
johyry <johannes.hyry@hotmail.com>
2018-11-01 11:32:56 +0200

    junit linecoverage 100%

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -18,6 +18,7 @@ public class VarastoTest {
     @Before
     public void setUp() {
         varasto = new Varasto(10);
+
     }
 
     @Test
@@ -64,5 +65,41 @@ public class VarastoTest {
         // varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
+
+    @Test
+    public void lisaysLiikaaSaldoa() {
+        varasto.lisaaVarastoon(100);
+
+        assertEquals(10, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void ottaaLiikaaSaldo() {
+        varasto.otaVarastosta(100);
+
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void ottaaNegatiivisenMaaran() {
+        assertEquals(0, varasto.otaVarastosta(-10), vertailuTarkkuus);
+    }
+
+    @Test
+    public void lisaaNegatiivisenMaaran() {
+        varasto.lisaaVarastoon(-10);
+
+    }
+
+    @Test
+    public void konstruktoriRiviKattavuus() {
+        Varasto test = new Varasto(0);
+        Varasto testi = new Varasto(100, -5);
+        Varasto testi1 = new Varasto(0, 0);
+        Varasto testi2 = new Varasto(10, 20);
+        testi1.toString();
+
+    }
+
 
 }
